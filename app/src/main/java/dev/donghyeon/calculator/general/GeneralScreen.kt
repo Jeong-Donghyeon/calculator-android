bcb54ba3e26177c9ea91b0987ca102eea4235737
Jeong-Donghyeon <dev.donghyeon@gmail.com>
2024-01-09 22:25:27 +0900

    일반 계산기 레이아웃 수정

--- app/src/main/java/dev/donghyeon/calculator/general/GeneralScreen.kt
+++ app/src/main/java/dev/donghyeon/calculator/general/GeneralScreen.kt
@@ -1,38 +1,28 @@
 package dev.donghyeon.calculator.general
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.donghyeon.calculator.theme.ColorSet
-import dev.donghyeon.calculator.theme.TextSet
+import dev.donghyeon.calculator.view.TitleView
+import dev.donghyeon.calculator.view.ViewButtonNumber
 
 @Preview
 @Composable
-fun Preview_GeneralScreen() =
-    GeneralScreen(
-        state =
-            GeneralData(
-                calculation = "1+1",
-                result = "2",
-            ),
-    )
+fun Preview_GeneralScreen() = GeneralScreen(state = GeneralData())
 
 @Composable
 fun GeneralScreen() {
@@ -48,217 +38,55 @@ fun GeneralScreen() {
 fun GeneralScreen(
     state: GeneralData,
     action: GeneralAction? = null,
+    menu: (() -> Unit)? = null,
 ) {
-    Column(
-        modifier =
-            Modifier
-                .background(ColorSet.container)
-                .fillMaxSize()
-                .padding(10.dp)
-                .padding(bottom = 20.dp),
-        verticalArrangement = Arrangement.spacedBy(5.dp),
-    ) {
+    Column(modifier = Modifier.background(ColorSet.container)) {
+        TitleView(title = "일반 계산기")
+        Box(modifier = Modifier.weight(1f)) {}
+        Row(verticalAlignment = Alignment.Bottom) {
+            ViewButtonNumber(
+                modifier = Modifier.padding(start = 10.dp),
+                text = "메뉴",
+                height = 40.dp,
+                size = 20.sp,
+                onClick = menu ?: {},
+            )
+        }
         Column(
             modifier =
                 Modifier
-                    .weight(1f)
-                    .padding(horizontal = 20.dp)
-                    .padding(bottom = 20.dp),
+                    .padding(10.dp)
+                    .padding(bottom = 10.dp),
         ) {
-            Box(modifier = Modifier.weight(1f))
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.End,
-                text = state.calculation,
-                style = TextSet.extraBold.copy(ColorSet.text, 30.sp),
-            )
-            Box(modifier = Modifier.weight(1f))
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.End,
-                text = state.result,
-                style = TextSet.extraBold.copy(ColorSet.text, 30.sp),
-            )
+            Keyboard()
         }
-        Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = {},
-            ) { Text(text = "") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = {},
-            ) { Text(text = "") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = {},
-            ) { Text(text = "") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = {},
-            ) { Text(text = "←") }
-        }
-        Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = {},
-            ) { Text(text = "C") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = {},
-            ) { Text(text = "") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("%") },
-            ) { Text(text = "%") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("÷") },
-            ) { Text(text = "÷") }
-        }
-        Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("7") },
-            ) { Text(text = "7") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("8") },
-            ) { Text(text = "8") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("9") },
-            ) { Text(text = "9") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("x") },
-            ) { Text(text = "x") }
-        }
-        Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("4") },
-            ) { Text(text = "4") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("5") },
-            ) { Text(text = "5") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("6") },
-            ) { Text(text = "6") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("-") },
-            ) { Text(text = "-") }
-        }
-        Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("1") },
-            ) { Text(text = "1") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("2") },
-            ) { Text(text = "2") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("3") },
-            ) { Text(text = "3") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("+") },
-            ) { Text(text = "+") }
-        }
-        Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("00") },
-            ) { Text(text = "00") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input("0") },
-            ) { Text(text = "0") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = { action?.input(".") },
-            ) { Text(text = ".") }
-            Button(
-                modifier =
-                    Modifier
-                        .weight(1f)
-                        .height(60.dp),
-                onClick = {},
-            ) { Text(text = "=") }
+    }
+}
+
+@Composable
+private fun Keyboard() {
+    Column(modifier = Modifier.height(420.dp)) {
+        listOf(
+            listOf("C", "<-", "->", "⌫"),
+            listOf("^", "(", ")", "/"),
+            listOf("7", "8", "9", "x"),
+            listOf("4", "5", "6", "-"),
+            listOf("1", "2", "3", "+"),
+            listOf("00", "0", ".", "="),
+        ).forEach {
+            Row(modifier = Modifier.weight(1f)) {
+                it.forEach {
+                    ViewButtonNumber(
+                        modifier =
+                            Modifier
+                                .weight(1f)
+                                .fillMaxHeight()
+                                .padding(2.dp),
+                        onClick = {},
+                        text = it,
+                    )
+                }
+            }
         }
     }
 }
