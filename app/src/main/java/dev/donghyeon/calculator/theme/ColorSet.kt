11cafd12a5d3bbefc582f0da14ef16d46e049d49
Jeong-Donghyeon <dev.donghyeon@gmail.com>
2024-01-21 19:21:34 +0900

    디자인 색상 수정

--- app/src/main/java/dev/donghyeon/calculator/theme/ColorSet.kt
+++ app/src/main/java/dev/donghyeon/calculator/theme/ColorSet.kt
@@ -4,10 +4,10 @@ import androidx.compose.ui.graphics.Color
 
 object ColorSet {
     val transparent: Color = Color.Transparent
-    val container: Color = Color.Black
+    val container: Color = Color(0xFF757575)
     val text: Color = Color.White
     val button: Color = Color.DarkGray
-    val select: Color = Color.Yellow
-    val result: Color = Color.Green
-    val bracket: Color = Color.Blue
+    val select: Color = Color(0xFFFFE13C)
+    val result: Color = Color.White
+    val bracket: Color = Color(0xFFFFE13C)
 }
